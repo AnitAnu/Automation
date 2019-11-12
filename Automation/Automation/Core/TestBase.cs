8b9a5a6172ca0ebcd58bad75bc8d779889609ce9
anita_biradar <anita_biradar@Dell.com>
2019-11-12 16:01:47 +0530

    Automation

---
+++ Automation/Automation/Core/TestBase.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automation.Core
+{
+    class TestBase
+    {
+        public static IWebDriver GetDriver()
+        {
+            var driver = new ChromeDriver();
+            var url = ConfigurationManager.AppSettings["baseUrl"].ToString();
+            driver.Navigate().GoToUrl(url);
+            return driver;
+        }
+    }
+}
---
+++ Automation/Automation/NavigateTest.cs
@@ -0,0 +1,42 @@
+﻿using Automation.Core;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automation
+{
+    [TestFixture]
+    public class NavigateTest
+    {
+        IWebDriver _driver = null;
+        [Test]
+        public void NavigateToDellPage()
+        {
+            _driver.FindElement(By.Id("dell")).Click();
+            Assert.IsTrue(_driver.Title.Contains("Computers and Electronics"));
+        }
+
+        [Test]
+        public void DellPage()
+        {
+            _driver.FindElement(By.Id("dell")).Click();
+            Assert.IsTrue(_driver.Title.Contains("Computers and Electronics"));
+        }
+
+        [SetUp]
+        public void Init()
+        {
+            _driver = TestBase.GetDriver();
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            _driver.Close();
+        }
+    }
+}
